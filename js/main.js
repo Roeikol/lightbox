ff8ada3ea4d5a6c0d5ea7f828dceeaac18ad4748
Roei <roeichwin@gmail.com>
2017-07-07 18:41:30 +0300

    Added colors to style and improve functions efficiency with added documentation

--- js/main.js
+++ js/main.js
@@ -1,36 +1,46 @@
-var imgContainer = $('<div>').attr('id', 'img_container');
-var img = $('<img>');
-var title = $('<p>');
-
-$('#gallery a').click(openViewport);
+//click functions listeners
+$('#gallery .image_link').on('click', openViewport);
 $("#right_arrow").on('click', scrollRight);
 $("#left_arrow").on('click', scrollLeft);
+$('#view_port').on('click', removeViewport);
 
 //open the viewport on picture click
-function openViewport(e){
+function openViewport(event){
 	event.preventDefault();
 	$('#view_port').show();
-	var imageLink = $(event.target).attr("src");
-	var imageTitle = $(event.target).attr("alt");
-
-	var newImg = img.attr("src", imageLink);
-	$('#view_port').append(imgContainer);
-	$('#img_container').append(newImg);
-
-	title.text(imageTitle);
-	$('#img_container').append(title);
+	createBigImg();
 }
+
+function createBigImg(){
+	var imgContainer = $('<div>').attr('id', 'img_container');
+	var bigImg = $('<img>').attr({
+		"class":"big_image",
+		"src":$(event.target).attr("src"),
+		"alt":$(event.target).attr("alt"),
+		"title":$(event.target).attr("title")
+	});
+	var imageDescription = $('<p>').text($(event.target).attr("title"));
+	addImgToViewport(imgContainer, bigImg, imageDescription);
+}
+
+function addImgToViewport(imgContainer, bigImg, imageDescription){
+	$('#view_port').append(imgContainer);
+	$('#img_container').append(bigImg);
+	$('#img_container').append(imageDescription);
+}
+
 //hide viewport when background is clicked
-$('#view_port').click(function(event){
+function removeViewport(event){
+	$('#img_container').remove();
 	$('#view_port').hide();
-})
+}
 
 //scrollbar move right when right arrow is clicked  
 function scrollRight(event){
-	console.log("f");
 	$('#gallery').animate({
 	scrollLeft: ($('#gallery').scrollLeft() + 200)});
 }
+
 //scrollbar move right when right arrow is clicked
 function scrollLeft(event){
 	$('#gallery').animate({
