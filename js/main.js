ee19259147f214265addcd4b1a688a6738ebbc1f
Roei <roeichwin@gmail.com>
2017-10-29 00:33:37 +0300

    add new css style and functions

--- js/main.js
+++ js/main.js
@@ -2,37 +2,33 @@
 $('#gallery .image_link').on('click', openViewport);
 $("#right_arrow").on('click', scrollRight);
 $("#left_arrow").on('click', scrollLeft);
-$('#view_port').on('click', removeViewport);
 
-//open the viewport on picture click
+//open the viewport on when picture is clicked
 function openViewport(event){
 	event.preventDefault();
 	$('#view_port').show();
-	createBigImg();
+	createLargeImg();
 }
-
-function createBigImg(){
+//creates a large image from the picture clicked
+function createLargeImg(){
 	var imgContainer = $('<div>').attr('id', 'img_container');
-	var bigImg = $('<img>').attr({
-		"class":"big_image",
-		"src":$(event.target).attr("src"),
-		"alt":$(event.target).attr("alt"),
-		"title":$(event.target).attr("title")
-	});
-	var imageDescription = $('<p>').text($(event.target).attr("title"));
-	addImgToViewport(imgContainer, bigImg, imageDescription);
+	var largeImg = $(event.target).addClass("large_image");
+	addImgToViewport(imgContainer, largeImg);
 }
-
-function addImgToViewport(imgContainer, bigImg, imageDescription){
+//display the large image in the viewport
+function addImgToViewport(imgContainer, largeImg){
 	$('#view_port').append(imgContainer);
-	$('#img_container').append(bigImg);
-	$('#img_container').append(imageDescription);
+	largeImg.clone().appendTo($('#img_container'));
+	removeViewport(imgContainer, largeImg);
 }
 
 //hide viewport when background is clicked
-function removeViewport(event){
-	$('#img_container').remove();
-	$('#view_port').hide();
+function removeViewport(imgContainer, img){
+	$('#view_port').on('click', function(){
+		img.removeClass("large_image");
+		imgContainer.remove();
+		$('#view_port').hide();
+	});
 }
 
 //scrollbar move right when right arrow is clicked  
